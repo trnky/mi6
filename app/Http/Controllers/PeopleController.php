d1412fa6186045d54a8477f745d8aea13f931f27
Martin <trnky76@hotmail.com>
2022-03-14 14:09:37 +0100

    react app connected to laravel

---
+++ app/Http/Controllers/PeopleController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PeopleController extends Controller
+{
+    
+    public function index()
+    {
+        return view('people.index');
+    }
+}
---
+++ resources/views/People/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('content')
+
+<h1>Welcome to MI6</h1>
+<div id="app"></div>
+
+@endsection
